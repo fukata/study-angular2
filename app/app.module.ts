f5fc1494c854b0db97fdf3976bfb231225aea185
fukata <tatsuya.fukata@gmail.com>
2016-12-17 00:24:22 +0700

    use hash route

--- app/app.module.ts
+++ app/app.module.ts
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
   ],
   declarations: [
     AppComponent,
